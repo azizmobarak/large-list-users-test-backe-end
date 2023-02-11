bcc2457d340103a9b5e23a16f9a382e6eb633edb
azizmobarak <mobarakaziz9@gmail.com>
2023-02-11 15:40:16 +0100

    reduce users

--- src/service/usersListByPage.ts
+++ src/service/usersListByPage.ts
@@ -7,7 +7,7 @@ export interface Users {
     image: string,
 }
 
-const totalUsers: number = 100_000;
+const totalUsers: number = 50_000;
 
 const getUsersList = (req: any, res: any) => {
     try {
